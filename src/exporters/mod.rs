3310204728cbda43634284db77205e735008146f
ollipa <25169984+ollipa@users.noreply.github.com>
2020-03-13 18:29:07 +0200

    Implement thread and channel logic

--- src/exporters/mod.rs
+++ src/exporters/mod.rs
@@ -1,1 +1,2 @@
-
+pub mod influxdb;
+pub mod stdout;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,42 @@
+use std::process;
+use std::{thread, time::Duration};
+
+use crossbeam_channel::{unbounded, Receiver, Sender};
+
 mod exporters;
 mod tools;
 
 fn main() {
-    println!("Hello, live share!");
+    println!("wiperf started");
+
+    ctrlc::set_handler(|| {
+        println!("received Ctrl+C!");
+        process::exit(0);
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    let (s, r) = unbounded();
+
+    // Ping
+    thread::spawn(move || send_stuff(s));
+    // Stdout
+    thread::spawn(move || receive_stuff(r));
+
+    loop {
+        thread::sleep(Duration::from_secs(1));
+    }
+}
+
+fn send_stuff(s: Sender<&str>) {
+    loop {
+        s.send("Hi!").unwrap();
+        thread::sleep(Duration::from_secs(1));
+    }
+}
+
+fn receive_stuff(r: Receiver<&str>) {
+    loop {
+        println!("Received: {:?}", r.recv());
+        thread::sleep(Duration::from_secs(1));
+    }
 }
