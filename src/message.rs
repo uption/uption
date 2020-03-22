e1ebd5de42c4bb37a313af50df77a5316ae87620
ollipa <25169984+ollipa@users.noreply.github.com>
2020-03-22 10:47:46 +0200

    Add timestamp to message

--- src/message.rs
+++ src/message.rs
@@ -1,19 +1,25 @@
-use serde::{Deserialize, Serialize};
 use std::fmt;
+
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Message {
+    timestamp: DateTime<Utc>,
     payload: String,
 }
 
 impl Message {
     pub fn new(payload: String) -> Message {
-        Message { payload }
+        Message {
+            timestamp: Utc::now(),
+            payload,
+        }
     }
 }
 
 impl fmt::Display for Message {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.payload)
+        write!(f, "{}, {}", self.timestamp.to_rfc3339(), self.payload)
     }
 }
