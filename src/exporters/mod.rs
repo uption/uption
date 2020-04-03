aa4862493f4d661d1b30da51f716a8a5476de939
ollipa <25169984+ollipa@users.noreply.github.com>
2020-04-03 15:38:45 +0300

    Implement exporter retry logic

--- src/exporters/mod.rs
+++ src/exporters/mod.rs
@@ -5,54 +5,142 @@ mod stdout;
 use std::io;
 use std::{thread, time::Duration};
 
+extern crate rand;
 use crossbeam_channel::Receiver;
+use rand::Rng;
 
 use crate::message::Message;
 pub use influxdb::InfluxDB;
 pub use stdout::Stdout;
 
+const ZERO: Duration = Duration::from_secs(0);
+
 pub struct ExporterScheduler {
-    exporter: Option<Box<dyn Exporter + Send>>,
+    exporter: Box<dyn Exporter + Send>,
+    receiver: Receiver<Message>,
+    retry_buffer: RetryItem,
 }
 
 impl ExporterScheduler {
-    pub fn new() -> ExporterScheduler {
-        ExporterScheduler { exporter: None }
-    }
-
-    pub fn register(&mut self, exporter: impl Exporter + Send + 'static) {
-        self.exporter.replace(Box::new(exporter));
-    }
-
-    pub fn start(&self, receiver: Receiver<Message>) {
-        if self.exporter.is_none() {
-            println!("No exporters configured!");
-            return;
+    pub fn new(
+        exporter: impl Exporter + Send + 'static,
+        receiver: Receiver<Message>,
+    ) -> ExporterScheduler {
+        ExporterScheduler {
+            exporter: Box::new(exporter),
+            receiver,
+            retry_buffer: RetryItem::new(Duration::from_secs(120)),
         }
+    }
+
+    pub fn start(&mut self) {
         println!("Exporter scheduler started");
 
         loop {
-            let msg = match receiver.recv() {
-                Ok(msg) => msg,
-                Err(_) => {
-                    println!("Collectors disconnected. Stopping exporter.");
-                    return;
-                }
+            let message = match self.receive() {
+                Some(message) => message,
+                None => break,
             };
 
-            match self.exporter.as_ref().unwrap().export(&msg) {
-                Ok(_) => {
-                    println!("Exported message from {} collector", msg.source);
-                }
-                Err(err) => {
-                    println!("Export error: {}", err);
-                    thread::sleep(Duration::from_secs(1));
-                }
+            self.export(message);
+            self.backoff_sleep();
+        }
+
+        println!("Collectors disconnected. Stopping exporter.");
+    }
+
+    fn receive(&mut self) -> Option<Message> {
+        match self.retry_buffer.take() {
+            Some(message) => Some(message),
+            None => match self.receiver.recv() {
+                Ok(message) => Some(message),
+                Err(_) => None,
+            },
+        }
+    }
+
+    fn export(&mut self, message: Message) {
+        match self.exporter.export(&message) {
+            Ok(_) => {
+                println!("Exported message from {} collector", message.source);
+                self.retry_buffer.decrement_error_count();
             }
+            Err(err) => self.handle_export_error(message, err),
+        }
+    }
+
+    fn handle_export_error(&mut self, message: Message, err: io::Error) {
+        println!("Export error: {}", err);
+        self.retry_buffer.set(message);
+        self.retry_buffer.increment_error_count();
+    }
+
+    fn backoff_sleep(&self) {
+        if self.retry_buffer.error_count > 0 {
+            let backoff = self.retry_buffer.backoff_duration(true);
+            println!("Exporting again in {:?}", backoff);
+            thread::sleep(backoff);
         }
     }
 }
 
 pub trait Exporter {
     fn export(&self, msg: &Message) -> Result<(), io::Error>;
+}
+
+struct RetryItem {
+    message: Option<Message>,
+    error_count: u64,
+    max_backoff: Duration,
+}
+
+impl RetryItem {
+    fn new(max_backoff: Duration) -> Self {
+        RetryItem {
+            message: None,
+            error_count: 0,
+            max_backoff,
+        }
+    }
+
+    fn take(&mut self) -> Option<Message> {
+        self.message.take()
+    }
+
+    fn set(&mut self, message: Message) {
+        self.message = Some(message);
+    }
+
+    fn increment_error_count(&mut self) {
+        if self.backoff_duration(false) < self.max_backoff {
+            self.error_count += 1;
+        }
+    }
+
+    fn decrement_error_count(&mut self) {
+        if self.error_count > 0 {
+            self.error_count -= 1;
+        }
+    }
+
+    fn backoff_duration(&self, jitter: bool) -> Duration {
+        match self.error_count {
+            0 => ZERO,
+            _ => {
+                let base = 200.0;
+                let multiplier = 1.5_f64;
+                let n = self.error_count as f64;
+
+                let mut backoff =
+                    (base * (multiplier.powf(n) - 1.0)).min(self.max_backoff.as_millis() as f64);
+
+                if jitter {
+                    let mut rng = rand::thread_rng();
+                    backoff += backoff * rng.gen_range(-0.1, 0.1);
+                }
+
+                Duration::from_millis(backoff as u64)
+            }
+        }
+    }
 }
--- src/uption.rs
+++ src/uption.rs
@@ -1,10 +1,11 @@
 use std::thread;
 
-use crossbeam_channel::unbounded;
+use crossbeam_channel::{unbounded, Receiver};
 
 use crate::collectors::{CollectorScheduler, Ping, HTTP};
 use crate::config::{ExporterSelection, UptionConfig};
 use crate::exporters::{ExporterScheduler, InfluxDB, Stdout};
+use crate::message::Message;
 
 const UPTION_VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
 
@@ -19,16 +20,15 @@ impl Uption {
 
     pub fn start(&self) {
         println!("Uption v{} started", UPTION_VERSION.unwrap_or("-unknown"));
-        let (s, r) = unbounded();
+        let (sender, receiver) = unbounded();
 
         let mut collector_scheduler = CollectorScheduler::new(self.config.collectors.interval);
         self.register_ping_collectors(&mut collector_scheduler);
         self.register_http_collectors(&mut collector_scheduler);
-        let collector_scheduler = thread::spawn(move || collector_scheduler.start(s));
+        let collector_scheduler = thread::spawn(move || collector_scheduler.start(sender));
 
-        let mut export_scheduler = ExporterScheduler::new();
-        self.register_exporters(&mut export_scheduler);
-        let export_scheduler = thread::spawn(move || export_scheduler.start(r));
+        let mut export_scheduler = self.create_export_scheduler(receiver);
+        let export_scheduler = thread::spawn(move || export_scheduler.start());
 
         collector_scheduler
             .join()
@@ -59,17 +59,20 @@ impl Uption {
         }
     }
 
-    fn register_exporters(&self, scheduler: &mut ExporterScheduler) {
+    fn create_export_scheduler(&self, receiver: Receiver<Message>) -> ExporterScheduler {
         let export = &self.config.exporters;
         match export.exporter {
-            ExporterSelection::InfluxDB => scheduler.register(InfluxDB::new(
-                export.influxdb.url.as_ref().unwrap(),
-                &export.influxdb.bucket.as_ref().unwrap(),
-                &export.influxdb.organization.as_ref().unwrap(),
-                &export.influxdb.token.as_ref().unwrap(),
-                export.influxdb.timeout,
-            )),
-            ExporterSelection::Stdout => scheduler.register(Stdout::new()),
-        };
+            ExporterSelection::InfluxDB => ExporterScheduler::new(
+                InfluxDB::new(
+                    export.influxdb.url.as_ref().unwrap(),
+                    &export.influxdb.bucket.as_ref().unwrap(),
+                    &export.influxdb.organization.as_ref().unwrap(),
+                    &export.influxdb.token.as_ref().unwrap(),
+                    export.influxdb.timeout,
+                ),
+                receiver,
+            ),
+            ExporterSelection::Stdout => ExporterScheduler::new(Stdout::new(), receiver),
+        }
     }
 }
